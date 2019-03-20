c9cc6464436651d42751c3beaa4309fefd370843
Alok Reddy <alokreddyk@gmail.com>
2019-03-20 18:29:05 +0530

    Updates

---
+++ precache-manifest.a93117279aef54c35b5456a9d9b0244f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forms/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7b433bcb0485557f4f92",
+    "url": "/forms/static/js/runtime~main.93bf94d6.js"
+  },
+  {
+    "revision": "02adf21ce5875fbef2af",
+    "url": "/forms/static/js/main.d9981475.chunk.js"
+  },
+  {
+    "revision": "ced6db8f4e0b4069513a",
+    "url": "/forms/static/js/2.760b29d1.chunk.js"
+  },
+  {
+    "revision": "02adf21ce5875fbef2af",
+    "url": "/forms/static/css/main.d0b20e60.chunk.css"
+  },
+  {
+    "revision": "ced6db8f4e0b4069513a",
+    "url": "/forms/static/css/2.f4ede888.chunk.css"
+  },
+  {
+    "revision": "b526f5ba8003a814cc11517ed2da7bc9",
+    "url": "/forms/index.html"
+  }
+];
